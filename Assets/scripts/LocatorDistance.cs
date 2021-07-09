7a2e002beecafcd22e4bf00849f8df0464f05cf8
Roma <461noname.error4o4@gmail.com>
2021-07-09 14:09:03 +0300

    new

---
+++ Assets/scripts/LocatorDistance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Sound))]
+public class LocatorDistance : MonoBehaviour
+{
+    [SerializeField] private Transform _boomb;
+    private Sound _sound;
+    private bool _playSound = true;
+    private float _timeDelay;
+    private void Start()
+    {
+        _sound = GetComponent<Sound>();
+    }
+    private void Update()
+    {
+        if(_playSound)
+        {
+            _timeDelay = Vector2.Distance(transform.position, _boomb.position);
+            StartCoroutine(nameof(LounthSound));
+        }
+    }
+    private IEnumerator LounthSound()
+    {
+        _playSound = false;
+        _sound.PlayBoom();
+        yield return new WaitForSeconds(_timeDelay);
+        _playSound = true;
+    }
+}
---
+++ Assets/scripts/MoveLocator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocator : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private void Update()
+    {
+        var inputHorizpntal = Input.GetAxis("Horizontal")  * _speed* Time.deltaTime;
+        transform.Translate(Vector2.right * inputHorizpntal);
+    }
+}
